7ddb0e38385ba4d7175072aaa720d487b4fa889d
JJimenez <ppjm@correo.ugr.es>
2014-06-23 14:26:00 +0200

    Commands for programmin funcionalaty
    Signed-off-by: JJimenez <ppjm@correo.ugr.es>

--- software/shell/shell.mk
+++ software/shell/shell.mk
@@ -1,4 +1,10 @@
 obj-$(CONFIG_WR_NODE) += \
 	shell/shell.o \
 	shell/cmd_sdb.o \
-	shell/cmd_help.o 
+	shell/cmd_write.o \
+	shell/cmd_read.o \
+	shell/cmd_wbr.o \
+	shell/cmd_wbw.o \
+	shell/cmd_setbase.o \
+	shell/cmd_setbsdb.o \
+	shell/cmd_help.o
